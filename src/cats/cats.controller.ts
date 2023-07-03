ad63d1dafa2533d863620df58e9f5b784daa26c0
mochi-ol <mochialcohol@gmail.com>
2023-07-03 18:05:34 +0900

    Dependency injection

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -9,7 +9,7 @@ export class CatsController {
 
   @Post()
   async create(@Body() createCatDto: CreateCatDto) {
-    this.catsService.create(createCatDto)
+    this.catsService.create(createCatDto) //this.catService に CatServiceが入る：コンストラクタインジェクション
   }
 
   @Get()
