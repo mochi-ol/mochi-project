ff708ad09d2087ec69bddad31f5e1d678dde4689
mochi-ol <mochialcohol@gmail.com>
2023-07-03 14:25:36 +0900

    full resource sample

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,14 +1,33 @@
-import { Controller, Get, Post } from '@nestjs/common';
+
+import { Controller, Get, Query, Post, Body, Put, Param, Delete } from '@nestjs/common';
+import { CreateCatDto } from './create-cat.dto';
 
 @Controller('cats')
 export class CatsController {
   @Post()
-  create(): string {
+  create(@Body() createCatDto: CreateCatDto) {
     return 'This action adds a new cat';
   }
 
   @Get()
-  findAll(): string {
-    return 'This action returns all cats';
+  findAll(@Query() query: ListAllEntities) {
+    return `This action returns all cats (limit: ${query.limit} items)`;
   }
+
+  @Get(':id') //Route parameters GET /cats/1 でID 1のcatを取得
+  findOne(@Param('id') id: string) {
+    return `This action returns a #${id} cat`;
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateCatDto: UpdateCatDto) {
+    return `This action updates a #${id} cat`;
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `This action removes a #${id} cat`;
+  }
+
 }
+// catsに対してCRUDのAPIが作れました
