b1b3f3aed081d2596b8d96683ec6659e56084beb
mochi-ol <mochialcohol@gmail.com>
2023-07-14 14:35:32 +0900

    add create users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+  @Get()
+  findAll() {
+    return this.usersService.findAll();
+  }
+
+  @Post()
+  create(@Body() createUser: CreateUserDto) {
+    return this.usersService.create(createUser);
+  }
+
+
+}
