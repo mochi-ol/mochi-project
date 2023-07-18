b7313a5fceebddab72188da3f7715005a3cb84c9
mochi-ol <mochialcohol@gmail.com>
2023-07-18 17:33:33 +0900

    regist TypeOrmModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,37 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CatsModule } from './cats/cats.module';
 import { UsersModule } from './users/users.module';
+import config from '../config/configuration';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 @Module({
-  imports: [CatsModule, UsersModule],
+  imports: [
+    AppModule,
+    CatsModule,
+    UsersModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [config],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('database.host'),
+        port: configService.get('database.port'),
+        username: configService.get('database.username'),
+        password: configService.get('database.password'),
+        database: configService.get('database.name'),
+        entities: ['dist/**entities/**/*.entity.ts'],
+      }),
+      inject: [ConfigService],
+    }),
+    UsersModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
