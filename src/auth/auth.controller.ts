2bee9799fce0d2235b001c3aadffe1f150a59358
mochi-ol <mochialcohol@gmail.com>
2023-07-25 15:31:44 +0900

    feat: make auth login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body, ValidationPipe, Param, Patch, Delete } from '@nestjs/common';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('login')
+  create(@Body(ValidationPipe) createUser: CreateUserDto) {
+    return this.authService.login(createUser);
+  }
+}
